b34accd5d77c7e9b6be02b8428589e2c72c1b7a6
darmijo15 <daniel.armijo@wsu.edu>
2022-03-20 22:36:10 -0700

    create an object quoteLibrary of quotes and their authors

---
+++ script.js
@@ -0,0 +1,7 @@
+let quoteLibrary = {
+        "Wayne Gretzky": "You miss 100% of the shots you don't take.",
+        "Lao Tzu": "The journey of a thousand miles begins with one step.",
+        "John Lennon": "Life is what happens when you're busy making other plans.",
+        "Mae West": "You only live once, but if you do it right, once is enough.",
+        "Albert Einstein": "Strive not to be a success, but rather to be of value."
+    };
