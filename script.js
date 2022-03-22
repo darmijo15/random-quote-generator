648e157b048d5ab8392d0daa0dc21b7f00b6c9dc
darmijo15 <daniel.armijo@wsu.edu>
2022-03-21 17:40:45 -0700

    add an click-eventlistener to the button running a function that selects a random quote and passes it to the html paragraph elements

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ let quoteLibrary = {
         "Albert Einstein": "Strive not to be a success, but rather to be of value."
     };
 
-const quotePara = document.getElementById("#quote-body");
-const authorPara = document.getElementById("#quote-author");
-const quoteButton = document.getElementById("#quote-btn");
+const quotePara = document.querySelector("#quote-body");
+const authorPara = document.querySelector("#quote-author");
+const quoteButton = document.querySelector("#quote-btn");
+
+quoteButton.addEventListener('click', function() {
+    let authorArray = Object.keys(quoteLibrary);
+    let authorIndex = Math.floor(Math.random() * authorArray.length);
+
+    let randomAuthor = authorArray[authorIndex];
+    let randomQuote = quoteLibrary[randomAuthor];
+
+    quotePara.innerHTML = `${randomQuote}`;
+    authorPara.innerHTML = `${randomAuthor}`;
+})
