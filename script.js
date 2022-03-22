3d5cf355214e831a1680fe36d8e5e24287431214
darmijo15 <daniel.armijo@wsu.edu>
2022-03-21 18:33:09 -0700

    rename the authorIndex to randomIndex to give the variable more meaning and add comments explaing the code

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// The first step is creating an object for the quotes to be used. 
+// This is done by passing the author as the key and the quote as the value.
 let quoteLibrary = {
         "Wayne Gretzky": "You miss 100% of the shots you don't take.",
         "Lao Tzu": "The journey of a thousand miles begins with one step.",
@@ -6,17 +9,37 @@ let quoteLibrary = {
         "Albert Einstein": "Strive not to be a success, but rather to be of value."
     };
 
+// Retrieve the paragraph and button elements from the DOM
 const quotePara = document.querySelector("#quote-body");
 const authorPara = document.querySelector("#quote-author");
 const quoteButton = document.querySelector("#quote-btn");
 
+// The button should generate a random quote when clicked, so
+// lets add an event listener.
 quoteButton.addEventListener('click', function() {
-    let authorArray = Object.keys(quoteLibrary);
-    let authorIndex = Math.floor(Math.random() * authorArray.length);
 
-    let randomAuthor = authorArray[authorIndex];
+    // Creating a function that returns a random object was much trickier
+    // than returning a random value from an array. With an array, it's as simple as
+    // generating a random number multiplied by the array length, and passing that 
+    // number as the index number. That same methodology can actually be used with
+    // objects.
+
+    // Create an array from the keys in the quoteLibrary
+    let authorArray = Object.keys(quoteLibrary);
+    // Generate an index number to be used in the array by multiplying 
+    // Math.random times the authorArray length and flooring it
+    let randomIndex = Math.floor(Math.random() * authorArray.length);
+
+    // Declare a variable, randomAuthor, that passes the random index number 
+    // into the authorArray
+    let randomAuthor = authorArray[randomIndex];
+    // Declare a variable, randomQuote, that passes the author (the object key)
+    // in order to grab the corresponding quote (the object value).
     let randomQuote = quoteLibrary[randomAuthor];
 
+    // Insert the quote into the quote-paragraph element so it displays 
+    // on the webpage.
     quotePara.innerHTML = `${randomQuote}`;
+    // Insert the quote's author into the author-paragraph element 
     authorPara.innerHTML = `${randomAuthor}`;
 })
