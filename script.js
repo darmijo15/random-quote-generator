55188ed0ab53b0755fd35de3b236f5ddf47c47da
darmijo15 <daniel.armijo@wsu.edu>
2022-03-21 23:19:08 -0700

    add more quotes to the quoteLibrary

--- script.js
+++ script.js
@@ -2,11 +2,31 @@
 // The first step is creating an object for the quotes to be used. 
 // This is done by passing the author as the key and the quote as the value.
 let quoteLibrary = {
-        "Wayne Gretzky": "You miss 100% of the shots you don't take.",
         "Lao Tzu": "The journey of a thousand miles begins with one step.",
         "John Lennon": "Life is what happens when you're busy making other plans.",
         "Mae West": "You only live once, but if you do it right, once is enough.",
-        "Albert Einstein": "Strive not to be a success, but rather to be of value."
+        "Albert Einstein": "Strive not to be a success, but rather to be of value.",
+        "Steven Jobs": "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+        "Benjamin Franklin": "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+        "Aristotle": "It is during our darkest moments that we must focus to see the light.",
+        "Anne Frank": "Whoever is happy will make others happy too.",
+        "Maya Angelou": "You will face many defeats in life, but never let yourself be defeated.",
+        "Abraham Lincoln": "In the end, it's not the years in your life that count. It's the life in your years.",
+        "Babe Ruth": "Never let the fear of striking out keep you from playing the game.",
+        "Helen Keller": "Life is either a daring adventure or nothing at all.",
+        "Thomas A. Edison": "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+        "Confucius": "Life is really simple, but we insist on making it complicated.",
+        "John Wooden": "Do not let making a living prevent you from making a life.",
+        "Jonathan Swift": "May you live all the days of your life.",
+        "Ray Bradbury": "Life is trying things to see if they work.",
+        "Winston S. Churchill": "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+        "Henry David Thoreau": "Success usually comes to those who are too busy to be looking for it.",
+        "Steve Jobs": "If you really look closely, most overnight successes took a long time.",
+        "Thomas Jefferson": "I find that the harder I work, the more luck I seem to have.",
+        "Barack Obama": "The real test is not whether you avoid this failure, because you won't. It's whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere.",
+        "Zig Ziglar": "Don't be distracted by criticism. Remember — the only taste of success some people get is to take a bite out of you.",
+        "Estee Lauder": "I never dreamed about success, I worked for it.",
+        "Vidal Sassoon": "The only place where success comes before work is in the dictionary."
     };
 
 // Retrieve the paragraph and button elements from the DOM
@@ -14,18 +34,12 @@ const quotePara = document.querySelector("#quote-body");
 const authorPara = document.querySelector("#quote-author");
 const quoteButton = document.querySelector("#quote-btn");
 
-// The button should generate a random quote when clicked, so
-// lets add an event listener.
+// Add an event listener to the button.
 quoteButton.addEventListener('click', function() {
-
-    // Creating a function that returns a random object was much trickier
-    // than returning a random value from an array. With an array, it's as simple as
-    // generating a random number multiplied by the array length, and passing that 
-    // number as the index number. That same methodology can actually be used with
-    // objects.
 
     // Create an array from the keys in the quoteLibrary
     let authorArray = Object.keys(quoteLibrary);
+    console.log(authorArray.length);
     // Generate an index number to be used in the array by multiplying 
     // Math.random times the authorArray length and flooring it
     let randomIndex = Math.floor(Math.random() * authorArray.length);
